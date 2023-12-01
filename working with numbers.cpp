f3d08c888cc9b7a31c8cf52025aaa70b88a19924
Nour Domani <138979883+NourDomani@users.noreply.github.com>
2023-12-01 10:46:19 +0300

    Add files via upload

---
+++ working with numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << 5+7 << endl;
+
+    cout << 5*7 << endl;
+
+    cout << 5-7 << endl;
+
+    cout << 5/7 << endl;
+
+    cout << 10%3 << endl;
+
+    cout << (5+7)*10 << endl;
+
+
+    int wnum = 5;
+    double cnum = 5.5;
+    wnum++;
+    cout << wnum << endl;
+
+
+    int vnum =5;
+    vnum--;
+    cout << vnum << endl;
+
+
+    int gnum = 5;
+    gnum +=90;
+    cout << gnum << endl;
+
+
+    cout << 10/3 << endl;
+    cout << 10.0/3.0 << endl;
+    return 0;
+}
